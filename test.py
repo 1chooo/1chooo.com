a669aa0f868dbcc0701d76459f684370e21a96b9
1chooo <hugo970217@gmail.com>
2024-01-07 00:01:21 +0800

    test: generate blog

---
+++ test.py
@@ -0,0 +1,63 @@
+import os
+import json
+import requests
+
+os.system("rm -rf ./blog")
+os.mkdir('./blog')
+
+GITHUB_TOKEN = ''
+GITHUB_USERNAME = ''
+
+try:
+
+    GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+
+except KeyError:
+    
+    with open('env.dev.json') as f:
+
+        env = json.loads(f.read())
+
+        GITHUB_TOKEN = env.get('GITHUB_TOKEN')
+
+with open('env.json') as f:
+
+    env = json.loads(f.read())
+
+    GITHUB_USERNAME = env.get('GITHUB_USERNAME')
+
+r = requests.get(f'https://api.github.com/users/{GITHUB_USERNAME}/gists', headers = {
+    "Authorization": f"token {GITHUB_TOKEN}"
+})
+
+posts = r.json()
+
+template = ''
+
+with open('./blog_template.html') as f:
+
+    template = f.read()
+
+for post in posts:
+
+    title = ''
+    id = post.get('id')
+    print(id)
+    print(post.get('description'))
+    is_page = 'page' in post.get('description').split(' #')
+    tags = post.get('description').split(' #')[2:]
+
+    print(tags)
+
+    for name in post.get('files'):
+        title = name.split('.md')[0]
+    print(is_page)
+    if is_page:
+
+        os.mkdir(f'./blog/{title}')
+
+        with open(f'./blog/{title}/index.html', 'w+') as f:
+
+            f.write(template.replace('{{ id }}', id))
+
+        print("success")
