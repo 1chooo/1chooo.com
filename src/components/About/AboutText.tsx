45f2228502679c32ba6ccbb943f0c39216b93aed
1chooo <hugo970217@gmail.com>
2024-02-02 00:29:14 +0800

    update: AboutText

--- src/components/About/AboutText.tsx
+++ src/components/About/AboutText.tsx
@@ -12,45 +12,29 @@ const AboutText: FunctionComponent = ({ }) => {
       </p>
 
       <p>
-        Hi, this is Hugo ChunHo Lin,
-        a 4th-year student at
+        I'm a 4th-year student at
         <a
           href="https://www.ncu.edu.tw/"
           className="code-highlight-yellow"
           target="_blank"
           rel="noreferrer"
-        > National Central University (NCU)
-        </a>,
-        driven by a <em><strong>genuine passion </strong></em>
+        > National Central University 🐿️
+        </a>, fueled by a <em><strong>sincere passion </strong></em>
         for the field of <strong>Software Engineering 💻</strong>.
       </p>
 
       <p>
-        I am currently preparing my application for a Master's Degree,
-        while simultaneously serving as a
-        <strong><a
-          href="https://aws.amazon.com/tw/education/awseducate/"
-          className="code-highlight-yellow"
-          target="_blank"
-          rel="noreferrer"
-        > Amazon Web Services (AWS) Educate
-        </a> Cloud Ambassador -- Tech Support </strong>
-        and an <strong>Ex Software Engineering (SWE) Intern at <a
-          href="https://www.pegatroncorp.com/"
-          className="code-highlight-yellow"
-          target="_blank"
-          rel="noreferrer"
-        >Pegatron</a></strong>,
-        turning complex problems into simple, beautiful and intuitive designs.
+        I do <strong>Web Development and Cloud Development </strong> 
+        with a focus on creating APIs and handling backend tasks using Python, GO, and AWS.
+        In general, I define new problems and find existing problems, 
+        transforming solutions into helpful documents or articles to assist everyone in the process,
+        and eventually apply them to make social impacts.
       </p>
 
       <p>
-        As part of my preparation, I'm honing my programming skills using the <strong><code className="code-highlight-yellow">GO</code></strong> language for <strong>Web Development,</strong> focusing on <strong>API creation and backend tasks.</strong> Additionally, I'm delving into <strong>automated deployment processes</strong> to grasp fundamental concepts of <strong><code className="code-highlight-yellow">Kubernetes</code> and <code className="code-highlight-yellow">Cloud Dev.</code> workflows.</strong>
+        In my spare time, I do street photography 📷 and consistently share my findings on GitHub with Global.
       </p>
 
-      <p>
-        Lastly, I would like to demonstrate my enthusiastic <code className="code-highlight-yellow">Open-Source</code> spirit, as I am eager to share my new knowledge and what I have learned. Thank you all for being here.
-      </p>
     </section>
   );
 }
