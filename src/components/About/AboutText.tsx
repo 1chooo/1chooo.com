6fad8e7fdc045a15143ae9d36bdf6147249d1af8
1chooo <hugo970217@gmail.com>
2024-02-02 01:27:59 +0800

    update: About Text

--- src/components/About/AboutText.tsx
+++ src/components/About/AboutText.tsx
@@ -12,7 +12,7 @@ const AboutText: FunctionComponent = ({ }) => {
       </p>
 
       <p>
-        I'm a 4th-year student at
+        I'm <strong>Hugo ChunHo Lin</strong>, a 4th-year student at
         <a
           href="https://www.ncu.edu.tw/"
           className="code-highlight-yellow"
@@ -24,15 +24,17 @@ const AboutText: FunctionComponent = ({ }) => {
       </p>
 
       <p>
-        I do <strong>Web Development and Cloud Development </strong> 
-        with a focus on creating APIs and handling backend tasks using Python, GO, and AWS.
+        <em>I do <strong>Web Development and Cloud Development </strong> 
+        with a focus on <strong>creating APIs and handling backend tasks </strong> 
+        using <code className="code-highlight-yellow">Python, Gin, and AWS</code>. </em>
+        
         In general, I define new problems and find existing problems, 
         transforming solutions into helpful documents or articles to assist everyone in the process,
         and eventually apply them to make social impacts.
       </p>
 
       <p>
-        In my spare time, I do street photography 📷 and consistently share my findings on GitHub with Global.
+        In my spare time, I do <em>street photography 📷</em> and <em>consistently share my findings on GitHub with Global 🌏.</em>
       </p>
 
     </section>
