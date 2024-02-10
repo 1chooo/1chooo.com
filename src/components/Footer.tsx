1044724c93e184aaf4a1997e7418713d5807446e
1chooo <hugo970217@gmail.com>
2024-02-11 01:15:42 +0800

    update: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
-function Footer() {
+const Footer: React.FC = ( ) => {
     return (
         <footer className="footer">
 
             <section className="footer">
-                <p className="copyright">Copyright © 2024 Share ❤️ via Hugo C. H. Lin </p>
+                <p className="copyright">Copyright © 2024 Share ❤️ via Hugo ChunHo Lin</p>
                 <p className="siteInfo">''𝘿𝙧𝙚𝙖𝙢𝙨 𝙘𝙤𝙢𝙚 𝙩𝙧𝙪𝙚, 𝙬𝙝𝙚𝙣 🫵🏻 𝙙𝙤𝙣’𝙩 𝙨𝙡𝙚𝙚𝙥…''</p>
             </section>
+
         </footer>
     );
 }
