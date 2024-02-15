0b861df3cfcce091a7558d202defe2d9e308514f
1chooo <hugo970217@gmail.com>
2024-02-16 00:02:39 +0800

    update: About Data #18

--- src/components/About/LifeStyles.tsx
+++ src/components/About/LifeStyles.tsx
@@ -1,7 +1,7 @@
-import writingImage from "../../Assets/images/icons/writing.png";
-import dumbBellImage from "../../Assets/images/icons/dumbbell.png";
-import developerImage from "../../Assets/images/icons/developer.png";
-import cameraImage from "../../Assets/images/icons/camera.png";
+import {
+  LifeStyle,
+  lifestyles
+} from '../Config/AboutData';
 
 
 const LifeStyles: React.FC = () => {
@@ -13,87 +13,26 @@ const LifeStyles: React.FC = () => {
       </p>
 
       <ul className="service-list">
-
-        <li className="service-item">
-
-          <div className="service-icon-box">
-            <img
-              src={developerImage}
-              alt="Developer"
-              width="30"
-            />
-          </div>
-
-          <div className="service-content-box">
-            <h4 className="h4 service-item-title">Open Source Contributor</h4>
-
-            <p className="service-item-text">
-              Actively contributing to open source projects on GitHub.
-            </p>
-          </div>
-
-        </li>
-
-        <li className="service-item">
-
-          <div className="service-icon-box">
-            <img
-              src={writingImage}
-              alt="Writing"
-              width="30"
-            />
-          </div>
-
-          <div className="service-content-box">
-            <h4 className="h4 service-item-title">Content Creator</h4>
-
-            <p className="service-item-text">
-              Love to share my knowledge and experience with others.
-            </p>
-          </div>
-
-        </li>
-
-        <li className="service-item">
-
-          <div className="service-icon-box">
-            <img
-              src={dumbBellImage}
-              alt="Dumbbell"
-              width="30"
-            />
-          </div>
-
-          <div className="service-content-box">
-            <h4 className="h4 service-item-title">Workouts</h4>
-
-            <p className="service-item-text">
-              Basketball and weight training defines my active workout lifestyle.
-            </p>
-          </div>
-
-        </li>
-
-        <li className="service-item">
-
-          <div className="service-icon-box">
-            <img
-              src={cameraImage}
-              alt="Camera"
-              width="30"
-            />
-          </div>
-
-          <div className="service-content-box">
-            <h4 className="h4 service-item-title">Photography</h4>
-
-            <p className="service-item-text">
-              Sky brings freedom; streets, a reminder of others' contributions.
-            </p>
-          </div>
-
-        </li>
-
+        {lifestyles.map((
+          lifestyle: LifeStyle,
+          index: number
+        ) => (
+          <li
+            className="service-item"
+            key={index}>
+            <div className="service-icon-box">
+              <img
+                src={lifestyle.icon}
+                alt={lifestyle.title}
+                width="30"
+              />
+            </div>
+            <div className="service-content-box">
+              <h4 className="h4 service-item-title">{lifestyle.title}</h4>
+              <p className="service-item-text">{lifestyle.text}</p>
+            </div>
+          </li>
+        ))}
       </ul>
 
     </section>
