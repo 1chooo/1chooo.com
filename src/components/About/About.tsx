e1c81328444884997c87d6909afd734980513147
1chooo <hugo970217@gmail.com>
2024-02-25 02:03:14 +0800

    refactor: about header

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,18 +3,18 @@ import {
   useLocation
 } from 'react-router-dom';
 
+import { aboutHeader } from "../../Config/AboutData";
 
 import NavBar from "../Navbar";
 import Sidebar from "../SideBar/SideBar";
-import AboutHeader from "./AboutHeader";
 import AboutText from "./AboutText";
 import LifeStyles from "./LifeStyles";
 import Github from "./Github";
 import TechStack from "./TechStack";
-// import Clients from "./Clients";
+import Clients from "./Clients";
 
 
-const About: React.FC = ( ) => {
+const About: React.FC = () => {
 
   const location = useLocation();
 
@@ -30,14 +30,19 @@ const About: React.FC = ( ) => {
           className={`about ${location.pathname === '/' ? 'active' : ''}`}
           data-page="about"
         >
-          <AboutHeader />
+
+          <header>
+            <h2 className="h2 article-title">
+              {aboutHeader}
+            </h2>
+          </header>
 
           <AboutText />
 
           <TechStack />
 
           <LifeStyles />
-          
+
           <Github />
 
           {/* <Clients /> */}
