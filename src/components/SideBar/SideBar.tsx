906879ed05ed452f8af5f85068680297b2e87679
1chooo <hugo970217@gmail.com>
2024-04-09 13:16:30 +0800

    feat: blog icon

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -9,7 +9,7 @@ import { MdExpandMore } from "react-icons/md";
 import SocialList from './SocialList';
 import ContactsList from './ContactsList';
 
-const SideBar: React.FC = ( ) => {
+const SideBar: React.FC = () => {
 
   // Sidebar state
   const [isActive, setIsActive] = useState(false);
@@ -26,21 +26,40 @@ const SideBar: React.FC = ( ) => {
   const sidebarClassName = `sidebar ${isActive ? 'active' : ''}`;
 
   return (
-    <aside className={sidebarClassName} ref={sidebarRef} data-sidebar>
+    <aside
+      className={sidebarClassName}
+      ref={sidebarRef}
+      data-sidebar
+    >
 
       <div className="sidebar-info">
         <figure className="avatar-box">
-          <img id="profile-img" src={profileImage} alt="Hugo ChunHo Lin" width="80" />
+          <img
+            id="profile-img"
+            src={profileImage}
+            alt="Hugo ChunHo Lin"
+            width="80"
+            loading="lazy"
+          />
         </figure>
 
         <div className="info-content">
-          <h1 className="name" title="Hugo ChunHo Lin">
+          <h1
+            className="name"
+            title="Hugo ChunHo Lin"
+          >
             Hugo ChunHo Lin
           </h1>
-          <p className="title"><strong>Day ONE ⚡️</strong></p>
+          <p className="title">
+            <strong>Day ONE ⚡️</strong>
+          </p>
         </div>
 
-        <button className="info_more-btn" onClick={sidebarBtnClickHandler} data-sidebar-btn>
+        <button
+          className="info_more-btn"
+          onClick={sidebarBtnClickHandler}
+          data-sidebar-btn
+        >
           <span>Show Contacts</span>
           <MdExpandMore />
         </button>
