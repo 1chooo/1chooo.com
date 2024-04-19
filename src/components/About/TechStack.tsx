25415e32f225761a13c1a5611ed7f929abf8b610
1chooo <hugo970217@gmail.com>
2024-04-19 20:16:50 +0800

    feat: update tech stack icons

--- src/components/About/TechStack.tsx
+++ src/components/About/TechStack.tsx
@@ -1,129 +1,29 @@
+import {
+  programmingLanguage, devOps, TechItem
+} from "../../Config/About/TechStack";
+
 const TechStack: React.FC = () => {
+
+  const renderTechStackList = (techStack: TechItem[]) => {
+    return (
+      <ul className="techstack-list has-scrollbar">
+        {techStack.map((item: TechItem, index: number) => (
+          <li key={index} className="techstack-item">
+            <img src={item.iconUrl} alt={item.name} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
     <section className="techstack">
-
       <p>
         <h3><code> $ ls -al Tech Stack</code></h3>
       </p>
 
-
-      <ul className="techstack-list has-scrollbar">
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=python"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=go"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=react"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=ts"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=js"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=cpp"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=c"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=java"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=latex"
-          />
-        </li>
-
-      </ul>
-
-      <ul className="techstack-list has-scrollbar">
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=linux"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=githubactions"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=aws"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=git"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=docker"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=fastapi"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=kubernetes"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=flask"
-          />
-        </li>
-
-        <li className="techstack-item">
-          <img
-            src="https://skillicons.dev/icons?i=bash"
-          />
-        </li>
-
-      </ul>
-
+      {renderTechStackList(programmingLanguage)}
+      {renderTechStackList(devOps)}
     </section>
   );
 }
