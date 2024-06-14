b361a93e6997430c128e429a2b12f00f1dfd4a75
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-06-15 00:48:23 +0800

    refactor: NavBar to use map for navigation items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,65 +1,29 @@
 import React from 'react';
-import {
-    Link, useLocation
-} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
     const location = useLocation();
+    const navItems = [
+        { path: '/', label: 'About' },
+        { path: '/resume', label: 'Resume' },
+        { path: '/portfolio', label: 'Portfolio' },
+        { path: '/blog', label: 'Blog' },
+        { path: '/contact', label: 'Contact' }
+    ];
 
     return (
         <nav className="navbar">
             <ul className="navbar-list">
-                <li className="navbar-item">
-                    <Link
-                        to="/"
-                        className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`}
-                    >About
-                    </Link>
-                </li>
-
-                <li className="navbar-item">
-                    <Link
-                        to="/resume"
-                        className={`navbar-link ${location.pathname === '/resume' ? 'active' : ''}`}
-                    >Resume
-                    </Link>
-                </li>
-
-                <li className="navbar-item">
-                    <Link
-                        to="/portfolio"
-                        className={`navbar-link ${location.pathname === '/portfolio' ? 'active' : ''}`}
-                    >Portfolio
-                    </Link>
-                </li>
-
-                <li className="navbar-item">
-                    <Link
-                        to="/blog"
-                        className={`navbar-link ${location.pathname === '/blog' ? 'active' : ''}`}
-                    >Blog
-                        
-                    </Link>
-                </li>
-
-                {/* <li className="navbar-item">
-                    <a
-                        href="https://blog.1chooo.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={`navbar-link ${location.pathname === '/blog' ? 'active' : ''}`}
-                    >
-                        Blog
-                    </a>
-                </li> */}
-
-                <li className="navbar-item">
-                    <Link
-                        to="/contact"
-                        className={`navbar-link ${location.pathname === '/contact' ? 'active' : ''}`}
-                    >Contact
-                    </Link>
-                </li>
+                {navItems.map(item => (
+                    <li key={item.path} className="navbar-item">
+                        <Link
+                            to={item.path}
+                            className={`navbar-link ${location.pathname === item.path ? 'active' : ''}`}
+                        >
+                            {item.label}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </nav>
     );
