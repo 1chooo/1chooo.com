e16baa032c264f7764cddeb65483c67b5f3f7f9e
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-06-18 00:04:01 +0800

    refactor: Router in App

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+import About from "./Page/About";
+import Contact from "./components/Contact/Contact";
+import Resume from "./Page/Resume";
+import Preloader from "../src/components/Pre";
+import Blog from "./Page/Blog";
+import Portfolio from "./Page/Portfolio";
+import ScrollToTop from "./components/ScrollToTop";
+import Docs from "./components/Docs/Docs";
+
+const routes = [
+  { path: "/", component: About },
+  { path: "/resume", component: Resume },
+  { path: "/portfolio", component: Portfolio },
+  { path: "/blog", component: Blog },
+  { path: "/contact", component: Contact },
+  { path: "/docs", component: Docs },
+];
+
+interface AppRoutesProps {
+  load: boolean;
+}
+
+const AppRoutes: React.FC<AppRoutesProps> = ({ load }) => {
+  return (
+    <Router>
+      <Routes>
+        {routes.map(({ path, component: Component }) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <>
+                <ScrollToTop />
+                <Preloader load={load} />
+                <Component />
+              </>
+            }
+          />
+        ))}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
