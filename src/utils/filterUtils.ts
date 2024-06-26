bf588a07630b04a3d7e41d37fffb91ce34b297e0
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-06-26 14:56:35 +0800

    refactor: handleBolgPaginationFilter to utils/

--- src/utils/filterUtils.ts
+++ src/utils/filterUtils.ts
@@ -10,3 +10,15 @@ export const filterFunc = (selectedValue: string, data: any[]) => {
     ? data
     : data.filter((item) => item.category.toLowerCase() === selectedValue.toLowerCase());
 };
+
+
+// src/utils/filterUtils.ts
+export const handleBlogPaginationFilter = (
+  tag: string,
+  setSelectedValue: React.Dispatch<React.SetStateAction<string>>,
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+) => {
+  handleItemClick(tag, setSelectedValue);
+  setCurrentPage(1); // Reset to first page on filter change
+  window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to the top
+};
