de72b328181ec31c4c63257ac76ecd8fdce058cf
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-18 20:10:37 +0800

    feat(md): support markdown CodeBlock

---
+++ src/components/CodeBlock.tsx
@@ -0,0 +1,30 @@
+import { CSSProperties, FunctionComponent } from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+type Props = {
+  children: string;
+  style?: CSSProperties;
+};
+
+const CodeBlock: FunctionComponent<Props> = ({ children }) => (
+  <SyntaxHighlighter
+    language="typescript"
+    customStyle={{
+      margin: '0 0 1.5rem',
+      padding: '1em',
+      backgroundColor: '#141414',
+      border: 'none',
+      borderRadius: 4,
+      lineHeight: 1.3,
+      overflowX: 'auto',
+    }}
+    codeTagProps={{ className: 'syntax-highlighter' }}
+    style={theme}
+    wrapLongLines={false}
+  >
+    {children}
+  </SyntaxHighlighter>
+);
+
+export default CodeBlock;
