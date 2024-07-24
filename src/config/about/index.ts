5ddda7c6eab9607d02ee4a7a5729edea34ff0cce
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-24 22:03:49 +0800

    feat(about): update intro

--- src/config/about/index.ts
+++ src/config/about/index.ts
@@ -37,9 +37,10 @@ export const abouts: IAbout = {
   "subHeader": "$ ls -al Hugo 👨🏻‍💻 (He/Him)",   // TODOs: Separate (He/Him) from subHeader
   "pronouns": "He/Him",
   "description": [
-    "I'm **Hugo ChunHo Lin**, a 4th-year student at [National Central University 🐿️](https://www.ncu.edu.tw/), fueled by a ***sincere passion*** for the field of **Software Engineering 💻.**",
-    "*I do **Web Development and Cloud Development** with a focus on **creating APIs and handling backend tasks** using `FastAPI, Gin, and AWS`. In general, I define new problems and find existing problems, transforming solutions into helpful documents or articles to assist everyone in the process, and eventually apply them to make social impacts.*",
-    "In my spare time, I do *street photography 📷* and *consistently share my findings on GitHub with Global 🌏*.",
+    "I'm **Hugo ChunHo Lin**, a graduate with a Bachelor's degree from [National Central University 🐿️](https://www.ncu.edu.tw/), driven by a *sincere passion* for **Software Engineering 💻.**",
+    // "*I do **Web Development and Cloud Development** with a focus on **creating APIs and handling backend tasks** using `FastAPI, Gin, and AWS`. In general, I define new problems and find existing problems, transforming solutions into helpful documents or articles to assist everyone in the process, and eventually apply them to make social impacts.*",
+    // "In my spare time, I do *street photography 📷* and *consistently share my findings on GitHub with Global 🌏*.",
+    "**`Self-motivated, Team player, Love coding. 👨🏻‍💻`**"
   ],
   "lifestyle": [
     {
