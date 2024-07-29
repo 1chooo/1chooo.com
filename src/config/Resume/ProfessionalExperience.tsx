f628675bd336d1871fdfcf8628b2eb20c7635103
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-30 01:03:15 +0800

    feat(resume): update work experience

--- src/config/Resume/ProfessionalExperience.tsx
+++ src/config/Resume/ProfessionalExperience.tsx
@@ -1,61 +1,52 @@
 export interface ProfessionalExperienceProps {
+  company: string;
   title: string;
+  date: string;
   description: JSX.Element[];
   location: string;
 }
 
 export const professionalExperiences: ProfessionalExperienceProps[] = [
   {
-    title: "eCloudvalley Digital Technology",
+    company: "eCloudvalley Digital Technology",
+    title: "Cloud Engineer Intern",
+    date: "Mar. 2024 - May. 2024",
     description: [
       (
-        <p>
-          <a
-            href="https://www.ecloudture.com/en/internship-program/"
-            className="profession-experience-title"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Cloud Engineer Intern
-          </a>
-          <span className="profession-experience-duration">
-            🗓️ Mar. 2024 - May. 2024
-          </span>
+        <p className="timeline-text">
+          <ul>
+            <li>
+              ☁️ Utilized a serverless architecture on <strong>AWS Lambda</strong>, performing <strong>troubleshooting with CloudWatch</strong>, deployed via <strong>AWS CDK</strong>, and integrated with <strong>DynamoDB and Bedrock</strong> services.
+            </li>
+            <li>
+              ☁️ <strong>Led a team of 5</strong> to enhance the existing ticket system with Claude, boosting <strong>cloud support enigineer efficiency by 80%.</strong> Integrated <strong>multi-language</strong> support and <strong>image recognition</strong>, achieving <strong>95%</strong> accuracy.
+            </li>
+            <li>
+              ☁️ Identified issues in <strong>10+</strong> mock-cases, like <strong>VPC entry failures, EC2 Apache Server restarts, and S3 photo access
+              problems.</strong> Documented solutions in a <strong><u><a
+                className="code-highlight-yellow"
+                href="https://github.com/1chooo/ecv-training-materials/tree/main/msp/aws_challenge"
+                target="_blank"
+                rel="noreferrer"
+              >Tech Note</a></u></strong> for sharing with interns.
+            </li>
+          </ul>
         </p>
-      ),
-      (
-        <br />
       ),
     ],
     location: "📍 New Taipei, Taiwan"
   },
   {
-    title: "Amazon Web Services",
+    company: "Amazon Web Services",
+    title: "Campus Ambassador",
+    date: "Aug. 2023 - Jan. 2024",
     description: [
-      (
-        <p>
-          <a
-            href="https://www.ecloudture.com/en/internship-program/"
-            className="profession-experience-title"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Cloud Ambassador Technical Support
-          </a>
-          <span className="profession-experience-duration">
-            🗓️ Aug. 2023 - Jan. 2024
-          </span>
-        </p>
-      ),
-      (
-        <br />
-      ),
       (
         <p className="timeline-text">
           <ul>
             <li>
-              ☁️ Educated <strong>700+</strong> cloud developers and achieved <strong>96%</strong> of 
-              user satisfaction by publishing and lecturing <strong>2+ technical workshops, 1 UAD</strong> on 
+              ☁️ Educated <strong>700+</strong> cloud developers and achieved <strong>96%</strong> of
+              user satisfaction by publishing and lecturing <strong>2+ technical workshops, 1 UAD</strong> on
               AWS cloud computing services <strong>(Lex, Amazon S3, Lambda, API Gateway)</strong>.
             </li>
             <li>
@@ -68,26 +59,10 @@ export const professionalExperiences: ProfessionalExperienceProps[] = [
     location: "📍 Taipei, Taiwan",
   },
   {
-    title: "PEGATRON Corporation",
+    company: "PEGATRON Corporation",
+    title: "Software Engineer Intern",
+    date: "Jul. 2023 - Aug. 2023",
     description: [
-      (
-        <p>
-          <a
-            href="https://www.ecloudture.com/en/internship-program/"
-            className="profession-experience-title"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Software Engineer Intern
-          </a>
-          <span className="timeline-text">
-            🗓️ Jul. 2023 - Aug. 2023
-          </span>
-        </p>
-      ),
-      (
-        <br />
-      ),
       (
         <p className="timeline-text">
           <ul>
@@ -96,12 +71,12 @@ export const professionalExperiences: ProfessionalExperienceProps[] = [
             </li>
             <li>
               🦾 Implementing <strong>Prompt Engineering and <code>LangChain</code></strong> allows
-              the <strong><a
+              the <strong><u><a
                 className="code-highlight-yellow"
                 href="https://www.nvidia.com/en-us/omniverse/"
                 target="_blank"
                 rel="noreferrer"
-              >NVIDIA Ominerve visual robot</a></strong> to function
+              >NVIDIA Ominerve visual robot</a></u></strong> to function
               through <strong>speech or text</strong>, leading to an <strong>83%</strong> improvement
               in scenario test support across <strong>6 to 11</strong> scenarios.
             </li>
