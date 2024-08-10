0aeed0e245a0a0840aadbeafe74b40921cf0e7e6
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-08-10 19:18:06 +0800

    refactor(rootlayout): add metadata, hello, favicon

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -1,20 +1,24 @@
-// pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.js (or app/_document.js)
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Preconnect for Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
+
+        {/* Favicon and Manifest */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
