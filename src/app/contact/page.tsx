923ba742b4a75823d8d54535241b8cb7689a1971
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-08-11 02:09:38 +0800

    feat(contact): add contact form and map box components

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,6 +4,8 @@ import { usePathname } from 'next/navigation';
 import SideBar from '@/components/side-bar';
 import NavBar from '@/components/nav-bar';
 import Header from '@/components/header';
+import MapBox from '@/components/contact/map-box';
+import { FaRegPaperPlane } from "react-icons/fa";
 
 
 const Contact = () => {
@@ -19,6 +21,46 @@ const Contact = () => {
           data-page="contact"
         >
           <Header title="Contact" />
+          <section className="contact-form">
+            <MapBox />
+            <h3 className="h3 form-title">Contact Form</h3>
+            <form action="#" className="form" data-form>
+              <div className="input-wrapper">
+                <input
+                  type="text"
+                  name="fullname"
+                  className="form-input"
+                  placeholder="Full name"
+                  required
+                  data-form-input
+                />
+                <input
+                  type="email"
+                  name="email"
+                  className="form-input"
+                  placeholder="Email address"
+                  required
+                  data-form-input
+                />
+              </div>
+              <textarea
+                name="message"
+                className="form-input"
+                placeholder="Your Message"
+                required
+                data-form-input
+              ></textarea>
+              <button
+                className="form-btn"
+                disabled
+                data-form-btn
+                onClick={() => alert('not implemented yet!')}
+              >
+                <FaRegPaperPlane />
+                <span>Send Message</span>
+              </button>
+            </form>
+          </section>
         </article>
       </div>
     </main>
