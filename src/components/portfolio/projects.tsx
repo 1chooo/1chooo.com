77c8bedf2bf5202c7e50864357a013f91d8a9e15
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-08-18 00:20:58 +0800

    refactor(portfolio): break down the Projects component into FilterList, FilterSelectBox, and ProjectItem (#105)

--- src/components/portfolio/projects.tsx
+++ src/components/portfolio/projects.tsx
@@ -1,96 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { MdExpandMore } from "react-icons/md";
-import { LuEye } from "react-icons/lu";
-import { projectTags, projectsData, Project } from '@/config/portfolio';
-import { filterCategory, handleItemClick } from '@/utils/filter-utils';
-import Image from 'next/image';
+import { projectsData } from '@/config/portfolio';
+import { filterCategory } from '@/utils/filter-utils';
+import FilterList from './filter-list';
+import FilterSelectBox from './filter-select-box';
+import ProjectItem from './project-item';
 
 const Projects: React.FC = () => {
-
   const [selectedValue, setSelectedValue] = useState('All');
   const [isSelectActive, setIsSelectActive] = useState(false);
 
   useEffect(() => {
-    handleItemClick('All', setSelectedValue);
+    setSelectedValue('All');
   }, []);
 
   const filteredProjects = filterCategory(selectedValue, projectsData);
 
   return (
     <section className="projects">
-
-      <ul className="filter-list">
-        {projectTags.map((tag, index) => (
-          <li className="filter-item" key={index}>
-            <button
-              className={`filter-btn ${selectedValue === tag ? 'active' : ''}`}
-              data-filter-btn
-              onClick={() => handleItemClick(tag, setSelectedValue)}
-            >
-              {tag}
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      <div className="filter-select-box">
-        <button
-          className={`filter-select ${isSelectActive ? 'active' : ''}`}
-          onClick={() => setIsSelectActive(!isSelectActive)}
-        >
-          <div className="select-value">
-            {selectedValue || 'Select category'}
-          </div>
-          <div className="select-icon">
-            <MdExpandMore />
-          </div>
-        </button>
-        {isSelectActive && (
-          <ul className="select-list">
-            {projectTags.map((tag: string) => (
-              <li className="select-item" key={tag}>
-                <button onClick={() => {
-                  handleItemClick(tag, setSelectedValue);
-                  setIsSelectActive(false);
-                }}>
-                  {tag}
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-
+      <FilterList
+        selectedValue={selectedValue}
+        setSelectedValue={setSelectedValue}
+      />
+      <FilterSelectBox
+        selectedValue={selectedValue}
+        isSelectActive={isSelectActive}
+        setIsSelectActive={setIsSelectActive}
+        setSelectedValue={setSelectedValue}
+      />
       <ul className="project-list">
-        {filteredProjects.map((project: Project, index: number) => (
-          <li
-            className="project-item active"
-            data-filter-item
-            data-category={project.category.toLowerCase()}
-            key={index}
-          >
-            <a
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <figure className="project-img">
-                <div className="project-item-icon-box">
-                  <LuEye />
-                </div>
-                <Image
-                  src={project.imageUrl}
-                  alt={project.title}
-                  loading="lazy"
-                />
-              </figure>
-              <h3 className="project-title">{project.title}</h3>
-              <p className="project-category">{project.category}</p>
-            </a>
-          </li>
+        {filteredProjects.map((project, index) => (
+          <ProjectItem project={project} key={index} />
         ))}
       </ul>
-
     </section>
   );
 };
