c8c9502f08b114cc5c716c255fe2871fe20ac7fb
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-08-24 15:39:37 +0800

    refactor: remove unused enum files and update config structures (#122)

---
+++ src/lib/constants.ts
@@ -0,0 +1,31 @@
+export const GITHUB_USERNAME = '1chooo';
+
+export const DEVOPS = {
+  linux: "linux",
+  aws: "aws",
+  azure: "azure",
+  githubactions: "githubactions",
+  docker: "docker",
+  fastapi: "fastapi",
+  kubernetes: "kubernetes",
+  flask: "flask",
+  gitlab: "gitlab",
+  redis: "redis"
+}
+
+export const PROGLANG =  {
+  python: "py",
+  go: "go",
+  java: "java",
+  cpp: "cpp",
+  c: "c",
+  react: "react",
+  typescript: "typescript",
+  javascript: "javascript",
+  flutter: "flutter",
+  bash: "bash"
+}
+
+
+
+
