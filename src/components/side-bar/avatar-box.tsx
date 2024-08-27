c9236129da361803a299067c47b07e7820d1a314
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-08-28 00:40:43 +0800

    refactor(avatar-box): separate avatar-box styles (#81)

--- src/components/side-bar/avatar-box.tsx
+++ src/components/side-bar/avatar-box.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
 
+import "@/styles/side-bar/avatar-box.css";
+
 const AvatarBox: React.FC = () => (
   <figure className="avatar-box">
     <Image
       id="profile-img"
       src="/images/profile.jpg"
       alt="Hugo ChunHo Lin"
-      width="80"
-      height="80"
+      width={80}
+      height={80}
       loading="lazy"
     />
   </figure>
