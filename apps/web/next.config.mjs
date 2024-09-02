a53b66cd6fe70abb735b425b6407f2002cda5319
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-09-03 00:09:43 +0800

    chore(deps-dev): init jest config (#177)

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,27 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://nextjs.org/docs/app/api-reference/next-config-js
+ */
+
+/**
+ * @type {import('next').NextConfig}
+ */
+
+
+const nextConfig = {
+  /* config options here */
+  images: {
+    domains: ['skillicons.dev', 'blog.1chooo.com'], // Use domains instead of remotePatterns
+    dangerouslyAllowSVG: true, // Enable SVG support for remote SVG images
+    unoptimized: true, // Disable image optimization to avoid issues with remote images
+  },
+  // https://nextjs.org/docs/architecture/nextjs-compiler
+  compiler: {
+    styledComponents: true,
+  },
+  // https://nextjs.org/docs/pages/building-your-application/configuring/mdx
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
+ 
+export default nextConfig
