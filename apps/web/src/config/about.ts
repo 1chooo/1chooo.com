fda5575a819d46c964897365077a318f2f1bfae9
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-09-03 01:50:34 +0800

    feat(about): update introduction (September, 03, 2024)

--- apps/web/src/config/about.ts
+++ apps/web/src/config/about.ts
@@ -12,9 +12,9 @@ export const abouts: About = {
   "pronouns": "He/Him",
   "introductions": [
     "I obtained my Bachelor's degree from [National Central University 🐿️](https://www.ncu.edu.tw/), driven by a *sincere passion* for **Software Engineering 💻.**",
-    `My current research interests include ***Programming Language Theory, Operating Systems, and Compilers.*** To gain practical experience, I developed my own ***<a href="https://en.wikipedia.org/wiki/Lisp_(programming_language)">Lisp</a>*** interpreter using **<a href="https://en.wikipedia.org/wiki/C_(programming_language)">C</a>** <sup>\[[Code](https://github.com/1chooo/compiler-101)\]</sup> and implemented [***Monkey Programming***](https://monkeylang.org/) Compiler with [**GO**](https://go.dev/) <sup>\[[Code](https://github.com/1chooo/monkey-prog-lang)\]</sup>.`,
+    `My current research interests include ***Programming Language Theory, Operating Systems, and Compilers.*** To gain practical experience, I developed my own ***<a href="https://en.wikipedia.org/wiki/Lisp_(programming_language)">Lisp</a>*** interpreter using **<a href="https://en.wikipedia.org/wiki/C_(programming_language)">C</a>** <sup>[[Code](https://github.com/1chooo/compiler-101)]</sup> and implemented [***Monkey Programming***](https://monkeylang.org/) Compiler with [**GO**](https://go.dev/) <sup>[[Code](https://github.com/1chooo/monkey-prog-lang)]</sup>.`,
     "In my spare time, I do workouts 💪🏻, street photography 📸 and tech content writing ✍🏻.",
-    "I will apply for Master's programs in Computer Science for 2024-25 cycle. Here is my current [CV](/cv.pdf) (August 2024).",
+    "I will apply for Master's programs in Computer Science for 2024-25 cycle. Here is my current [CV](/resume) <sup>[[Docs]](/cv.pdf)</sup> (August 2024).",
     "**`Self-motivated, Team player, Love coding 👨🏻‍💻`**"
   ],
   "lifestyle": [
