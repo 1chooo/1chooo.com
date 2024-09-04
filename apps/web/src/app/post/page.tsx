53cc8457817047e0f486f32d4777274aae5d110a
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-09-05 00:16:30 +0800

    feat(post): add mock `post` router for getting list of articles (#188)

--- apps/web/src/app/post/page.tsx
+++ apps/web/src/app/post/page.tsx
@@ -1,30 +1,51 @@
-'use client';
+import Link from 'next/link';
+import { getBlogPosts } from '../db/blog';
+import Header from '@/components/markdown/header';
 
-import React, { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-import { initializeCustomSelect, filterItemsByCategory } from '@/utils/dom-utils';
+export const metadata = {
+  title: 'Blog',
+  description: 'Read my thoughts on software development, design, and more.',
+};
 
-import BlogPosts from '@/components/posts/blog-post';
-import PageContent from '@/components/page-content';
-
-
-const Post = () => {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    initializeCustomSelect(filterItemsByCategory);
-  }, []);
+export default function BlogPage() {
+  let allBlogs = getBlogPosts();
 
   return (
-    <PageContent
-      documentTitle='Blog'
-      title="Hugo's Blog"
-      page="blog"
-      pathName={pathname}
-    >
-      <BlogPosts />
-    </PageContent >
+    <article>
+      <section>
+        <Header title="Hugo's Blog" />
+        {allBlogs
+          .sort((a, b) => {
+            if (
+              new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+            ) {
+              return -1;
+            }
+            return 1;
+          })
+          .map((post) => (
+            <Link
+              key={post.slug}
+              className="flex flex-col space-y-1 mb-4"
+              href={`/post/${post.slug}`}
+            >
+              <div className="w-full flex flex-col">
+                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                  {post.metadata.title}
+                </p>
+                {/* <Suspense fallback={<p className="h-6" />}>
+                <Views slug={post.slug} />
+              </Suspense> */}
+              </div>
+            </Link>
+          ))}
+      </section>
+    </article >
   );
 }
 
-export default Post;
+// async function Views({ slug }: { slug: string }) {
+//   let views = await getViewsCount();
+
+//   return <ViewCounter allViews={views} slug={slug} />;
+// }
