c9e17ba80e1cdb7ed050d4872b2fccbe13e7841b
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-09-05 00:02:10 +0800

    feat: add robots generator function (#185)

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://1chooo.com/sitemap.xml',
+    host: 'https://1chooo.com',
+  };
+}
