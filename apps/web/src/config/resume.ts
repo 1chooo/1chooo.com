79ad577b577d7a6b6af9703d6b70aa7c30c2c9dc
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-09-08 15:25:15 +0800

    feat(resume): update resume education and professional experience (#207)

--- apps/web/src/config/resume.ts
+++ apps/web/src/config/resume.ts
@@ -14,9 +14,9 @@ export const educations = {
       role: "Bachelors of Science in Atmospheric Science",
       duration: "Sep. 2020 — Jun. 2024",
       tasksMarkdown: `
-- 💻 **Minor Specialty:** Computer Science & Information Engineering
-- 💻 **Relevant Courses (Graduate):** The Attack and Defense of Computers, Software Engineering, Object-Oriented Analysis and Design.
-- 💻 **Relevant Courses (Undergraduate):** Linux And Edge Computing, Principles of Programming Languages, Operating System, Computer Organization, Computer Network, Algorithmics, Data Structures, Assembly Language and System Programming, Digital Design, Weather and Artificial Intelligence, Numerical Analysis.
+- **Minor Specialty:** Computer Science & Information Engineering
+- **Relevant Courses (Graduate):** The Attack and Defense of Computers, Software Engineering, Object-Oriented Analysis and Design.
+- **Relevant Courses (Undergraduate):** Linux And Edge Computing, Principles of Programming Languages, Operating System, Computer Organization, Computer Network, Algorithmics, Data Structures, Assembly Language and System Programming, Digital Design, Weather and Artificial Intelligence, Numerical Analysis.
     `,
     },
   ],
@@ -85,9 +85,9 @@ export const professionalExperiences = {
       role: "Cloud Engineer Intern",
       duration: "Mar. 2024 - May. 2024",
       tasksMarkdown: `
-- ☁️ Utilized a serverless architecture on **AWS Lambda**, performing **troubleshooting with CloudWatch**, deployed via **AWS CDK**, and integrated with **DynamoDB and Bedrock** services.
-- ☁️ **Led a team of 5** to enhance the existing ticket system with Claude, boosting **cloud support engineer efficiency by 80%**. Integrated **multi-language** support and **image recognition**, achieving **95%** accuracy.
-- ☁️ Identified issues in **10+** mock-cases, like **VPC entry failures, EC2 Apache Server restarts, and S3 photo access problems**. Documented solutions in a [Tech Note](https://github.com/1chooo/ecv-training-materials/tree/main/msp/aws_challenge) for sharing with interns.
+- Utilized a serverless architecture on **AWS Lambda**, performing **troubleshooting with CloudWatch**, deployed via **AWS CDK**, and integrated with **DynamoDB and Bedrock** services.
+- **Led a team of 5** to enhance the existing ticket system with Claude, boosting **cloud support engineer efficiency by 80%**. Integrated **multi-language** support and **image recognition**, achieving **95%** accuracy.
+- Identified issues in **10+** mock-cases, like **VPC entry failures, EC2 Apache Server restarts, and S3 photo access problems**. Documented solutions in a [Tech Note](https://github.com/1chooo/ecv-training-materials/tree/main/msp/aws_challenge) for sharing with interns.
       `,
     },
     {
@@ -96,8 +96,8 @@ export const professionalExperiences = {
       role: "Campus Ambassador",
       duration: "Aug. 2023 - Jan. 2024",
       tasksMarkdown: `
-- ☁️ Educated **700+** cloud developers and achieved **96%** of user satisfaction by publishing and lecturing **2+ technical workshops, 1 UAD** on AWS cloud computing services **(Lex, Amazon S3, Lambda, API Gateway).**
-- ☁️ **Developed** an **open-source** project called [AWS 101 LINE Bot](https://github.com/1chooo/aws-line-business-card-workshop) designed for integration with **AWS 101**.
+- Educated **700+** cloud developers and achieved **96%** of user satisfaction by publishing and lecturing **2+ technical workshops, 1 UAD** on AWS cloud computing services **(Lex, Amazon S3, Lambda, API Gateway).**
+- **Developed** an **open-source** project called [AWS 101 LINE Bot](https://github.com/1chooo/aws-line-business-card-workshop) designed for integration with **AWS 101**.
       `,
     },
     {
@@ -107,8 +107,8 @@ export const professionalExperiences = {
       duration: "Jul. 2023 - Aug. 2023",
       tasksMarkdown: `
 - **Smart Robot, Smart World 🦾:** Prompt-based Learning for manipulating with the Visual-World Robot
-- 🦾 Implementing **Prompt Engineering and \`LangChain\`** allows the [NVIDIA Ominerve visual robot](https://www.nvidia.com/en-us/omniverse/) to function through **speech or text**, leading to an **83%** improvement in scenario test support across **6 to 11** scenarios.
-- 🦾 During the **Proof of Concept (POC)** phase, transform the trained model into a **Web Application** with a user interface using **\`Gradio\`**, the UI toolkit for the **\`FastAPI\`** framework.
+- Implementing **Prompt Engineering and \`LangChain\`** allows the [NVIDIA Ominerve visual robot](https://www.nvidia.com/en-us/omniverse/) to function through **speech or text**, leading to an **83%** improvement in scenario test support across **6 to 11** scenarios.
+- During the **Proof of Concept (POC)** phase, transform the trained model into a **Web Application** with a user interface using **\`Gradio\`**, the UI toolkit for the **\`FastAPI\`** framework.
       `,
     },
   ],
