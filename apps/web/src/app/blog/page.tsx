070f130359435689d74d44270e2768644ed5f6d1
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-09-12 01:40:54 +0800

    feat(blog): add FilterList to pick up the specific category (#239)

--- apps/web/src/app/blog/page.tsx
+++ apps/web/src/app/blog/page.tsx
@@ -8,56 +8,77 @@ export const metadata = {
   description: 'Read my thoughts on software development, design, and more.',
 };
 
-export default function BlogPage() {
+// Define your blog tags here
+let blogTags: string[] = ['All'];
+
+export default function BlogPage({ searchParams }: { searchParams: { tag?: string } }) {
   let allBlogs = getBlogPosts();
+  blogTags = ['All', ...Array.from(new Set(allBlogs.map(post => post.metadata.category ?? '')))];
+  const selectedTag = searchParams.tag || 'All';
+
+  // Filter blogs based on the selected tag
+  const filteredBlogs = selectedTag === 'All'
+    ? allBlogs
+    : allBlogs.filter(post => post.metadata.category === selectedTag);
 
   return (
     <article>
       <PageHeader title="Hugo's Blog" />
-      <ul className="blog-posts-list">
-        {allBlogs
-          .sort((a, b) => {
-            if (
-              new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-            ) {
-              return -1;
-            }
-            return 1;
-          })
-          .map((post, index) => (
-            <li
-              key={index}
-              className="blog-post-item active"
-              data-category={post.metadata.category}
-            >
+      {/* Filter List */}
+      <section className="blog-posts">
+        <ul className="filter-list">
+          {blogTags.map((tag, index) => (
+            <li className="filter-item" key={index}>
               <Link
-                href={`/blog/${post.slug}`}
-                rel="noopener noreferrer"
+                href={`/blog?tag=${encodeURIComponent(tag || '')}`}
+                className={`filter-btn ${selectedTag === tag ? 'active' : ''}`}
               >
-                <figure className="blog-banner-box">
-                  <Image
-                    src={post.metadata.banner}
-                    alt={post.metadata.alt || 'Blog post image'}
-                    loading="lazy"
-                    width={1600}
-                    height={900}
-                  />
-                </figure>
-                <div className="blog-content">
-                  <div className="blog-meta">
-                    <p className="blog-category">{post.metadata.category}</p>
-                    <span className="dot"></span>
-                    <time dateTime={post.metadata.publishedAt}>
-                      {new Date(post.metadata.publishedAt).toLocaleDateString()}
-                    </time>
-                  </div>
-                  <h3 className="h3 blog-item-title">{post.metadata.title}</h3>
-                  <p className="blog-text">{post.metadata.summary}</p>
-                </div>
+                {tag}
               </Link>
             </li>
           ))}
-      </ul>
-    </article >
+        </ul>
+
+        <ul className="blog-posts-list">
+          {filteredBlogs
+            .sort((a, b) => {
+              if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+                return -1;
+              }
+              return 1;
+            })
+            .map((post, index) => (
+              <li
+                key={index}
+                className="blog-post-item active"
+                data-category={post.metadata.category}
+              >
+                <Link href={`/blog/${post.slug}`} rel="noopener noreferrer">
+                  <figure className="blog-banner-box">
+                    <Image
+                      src={post.metadata.banner}
+                      alt={post.metadata.alt || 'Blog post image'}
+                      loading="lazy"
+                      width={1600}
+                      height={900}
+                    />
+                  </figure>
+                  <div className="blog-content">
+                    <div className="blog-meta">
+                      <p className="blog-category">{post.metadata.category}</p>
+                      <span className="dot"></span>
+                      <time dateTime={post.metadata.publishedAt}>
+                        {new Date(post.metadata.publishedAt).toLocaleDateString()}
+                      </time>
+                    </div>
+                    <h3 className="h3 blog-item-title">{post.metadata.title}</h3>
+                    <p className="blog-text">{post.metadata.summary}</p>
+                  </div>
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </section>
+    </article>
   );
 }
