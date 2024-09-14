cc390eaff1bb2f648c7f63faa3cafc780bcc8a5c
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-09-15 00:02:36 +0800

    style(side-bar): update footer links to use Next.js Link component (#266)

--- apps/web/src/components/side-bar/footer.tsx
+++ apps/web/src/components/side-bar/footer.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import Link from "next/link";
+
 import "@/styles/side-bar/footer.css";
 
+// All rights reserved and subject to the Privacy Policy and Terms and Conditions.
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-copyright">© 2024 1chooo</div>
       <div className="footer-links">
-        <a href="https://docs.1chooo.com">Docs</a>
-        <a href="https://github.com/1chooo/1chooo.com">Vcard Theme</a>
+        <Link
+          href="https://docs.1chooo.com"
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          Docs
+        </Link>
+        <Link
+          href="https://github.com/1chooo/1chooo.com"
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          Vcard Theme
+        </Link>
       </div>
     </footer>
   );
