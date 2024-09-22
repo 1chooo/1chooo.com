deda2c7aa933a8e3ae727643827af8bf0d12af29
Muhammad Irfan <irfan.dev34@gmail.com>
2024-09-22 20:58:19 +0500

    feat(blogs): customize skeleton loader

--- apps/web/src/app/blog/page.tsx
+++ apps/web/src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import FilterSelectBox from "@/components/blog/filter-select-box";
 import FilterList from "@/components/blog/filter-list";
 import MarkdownRenderer from "@/components/markdown/markdown-renderer";
 import { getBlogPosts } from "@/lib/db/blog";
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 export const metadata = {
@@ -63,61 +63,62 @@ export default function BlogPage({
         <FilterSelectBox selectedTag={selectedTag} blogTags={blogTags} />
 
         <ul className="blog-posts-list">
-          {isLoading
-            ? Array.from({ length: POSTS_PER_PAGE }).map((_, index) => (
+          {isLoading ? (
+            <SkeletonTheme baseColor="#202020" highlightColor="#444">
+              {Array.from({ length: POSTS_PER_PAGE }).map((_, index) => (
                 <li key={index} className="blog-post-item skeleton">
                   <Skeleton height={200} className="blog-banner-skeleton" />
                   <div className="blog-content-skeleton">
-                    <Skeleton width={100} className="blog-category-skeleton" />
-                    <Skeleton width={180} className="blog-meta-skeleton" />
                     <Skeleton height={24} className="blog-title-skeleton" />
                     <Skeleton count={3} className="blog-summary-skeleton" />
                   </div>
                 </li>
-              ))
-            : paginatedBlogs.map((post, index) => (
-                <li
-                  key={index}
-                  className="blog-post-item active"
-                  data-category={post.metadata.category}
-                >
-                  <Link href={`/blog/${post.slug}`} rel="noopener noreferrer">
-                    <figure className="blog-banner-box">
-                      <Image
-                        src={post.metadata.banner}
-                        alt={post.metadata.alt || "Blog post image"}
-                        loading="lazy"
-                        width={1600}
-                        height={900}
-                      />
-                    </figure>
-                    <div className="blog-content">
-                      <div className="blog-meta">
-                        <p className="blog-category">
-                          {post.metadata.category}
-                        </p>
-                        <span className="dot"></span>
-                        <time dateTime={post.metadata.publishedAt}>
-                          {new Date(
-                            post.metadata.publishedAt
-                          ).toLocaleDateString("en-us", {
+              ))}
+            </SkeletonTheme>
+          ) : (
+            paginatedBlogs.map((post, index) => (
+              <li
+                key={index}
+                className="blog-post-item active"
+                data-category={post.metadata.category}
+              >
+                <Link href={`/blog/${post.slug}`} rel="noopener noreferrer">
+                  <figure className="blog-banner-box">
+                    <Image
+                      src={post.metadata.banner}
+                      alt={post.metadata.alt || "Blog post image"}
+                      loading="lazy"
+                      width={1600}
+                      height={900}
+                    />
+                  </figure>
+                  <div className="blog-content">
+                    <div className="blog-meta">
+                      <p className="blog-category">{post.metadata.category}</p>
+                      <span className="dot"></span>
+                      <time dateTime={post.metadata.publishedAt}>
+                        {new Date(post.metadata.publishedAt).toLocaleDateString(
+                          "en-us",
+                          {
                             month: "long",
                             day: "numeric",
                             year: "numeric",
-                          })}
-                        </time>
-                      </div>
-                      <h3 className="h3 blog-item-title">
-                        <MarkdownRenderer content={post.metadata.title} />
-                      </h3>
-                      <MarkdownRenderer
-                        className="blog-summary"
-                        content={post.metadata.summary}
-                      />
+                          }
+                        )}
+                      </time>
                     </div>
-                  </Link>
-                </li>
-              ))}
+                    <h3 className="h3 blog-item-title">
+                      <MarkdownRenderer content={post.metadata.title} />
+                    </h3>
+                    <MarkdownRenderer
+                      className="blog-summary"
+                      content={post.metadata.summary}
+                    />
+                  </div>
+                </Link>
+              </li>
+            ))
+          )}
         </ul>
         <div className="pagination">
           {Array.from({ length: totalPages }, (_, i) => i + 1).map(
@@ -128,7 +129,9 @@ export default function BlogPage({
                   pathname: "/blog",
                   query: { ...searchParams, page: pageNum.toString() },
                 }}
-                className={`pagination-btn ${pageNum === currentPage ? "active" : ""}`}
+                className={`pagination-btn ${
+                  pageNum === currentPage ? "active" : ""
+                }`}
               >
                 {pageNum}
               </Link>
