0faf9f0f4c9697badabd83cac674e02c206db120
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-18 18:15:05 +0800

    chore(docs): implement custom App component and migrate meta data to TypeScript

---
+++ apps/docs/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app'
+import type { ReactElement } from 'react'
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <Component
+      {...pageProps}
+    />
+  )
+}
