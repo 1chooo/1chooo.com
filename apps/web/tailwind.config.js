899c4f29422673fa43ed8b4a49c8617eea9a6d42
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2024-11-27 22:28:52 +0800

    feat(tailwind): integrate Tailwind CSS and refactor H4 component styles (#511) (#512)

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-
+      colors: {
+        "jet": 'hsl(0, 0%, 22%)',
+        "onyx": 'hsl(240, 1%, 17%)',
+        'eerie-black-1': 'hsl(240, 2%, 13%)',
+        'eerie-black-2': 'hsl(240, 2%, 12%)',
+        'smoky-black': 'hsl(0, 0%, 7%)',
+        'white-1': 'hsl(0, 0%, 100%)',
+        'white-2': 'hsl(0, 0%, 98%)',
+        'orange-yellow-crayola': 'hsl(45, 100%, 72%)',
+        'vegas-gold': 'hsl(45, 54%, 58%)',
+        'light-gray': 'hsl(0, 0%, 84%)',
+        'light-gray-70': 'hsla(0, 0%, 84%, 0.7)',
+        'bittersweet-shimmer': 'hsl(0, 43%, 51%)',
+      },
     },
   },
   plugins: [],
