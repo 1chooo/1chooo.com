6d4aec1970c73a96db9c46ee60dd41b17017a8df
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2024-11-28 16:36:03 +0800

    refactor(page-header): update title styles for consistency and remove unused CSS (#519) (#520)
    * refactor(page-header): update title styles for consistency and remove unused CSS (#519)
    
    * fix(portfolio): update title class for consistency and improved styling

--- apps/web/src/components/page-header.tsx
+++ apps/web/src/components/page-header.tsx
@@ -1,12 +1,10 @@
-'use client';
-
-import React from 'react';
+import React from "react";
 
 const PageHeader = ({ header }: { header: string }) => (
   <header>
-    <h2 className="h2 article-title">
+    <h1 className="relative pb-[7px] mb-[30px] font-semibold text-3xl text-white-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-[30px] after:h-[3px] after:rounded-sm  after:bg-text-gradient-yellow sm:pb-[15px] sm:after:w-[40px] sm:after:h-[5px] md:pb-[20px]">
       {header}
-    </h2>
+    </h1>
   </header>
 );
 
