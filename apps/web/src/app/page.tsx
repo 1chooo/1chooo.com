3bcd7ed93f84c51636cef6f33943294c2b9b50d1
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2024-11-29 19:44:07 +0800

    feat(about): add LatestArticles component to display recent blog posts and update About page layout (#533) (#534)

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,29 +1,24 @@
 import type { Metadata } from "next";
-import Link from "next/link";
-import Image from "next/image";
-import { LuEye } from "react-icons/lu";
 import AboutText from '@/components/about/about-text';
 import GitHubStats from '@/components/about/github-stats';
 import TechStack from '@/components/about/tech-stack';
 import LifeStyles from '@/components/about/life-styles';
 import PageHeader from '@/components/page-header';
 import AboutHeader from '@/components/about/about-header';
-import MarkdownRenderer from "@/components/markdown/markdown-renderer";
-import { FaRegPenToSquare } from "react-icons/fa6";
 import SeeMoreButton from "@/components/about/see-more-btn";
+import LatestArticles from "@/components/about/latest-articles";
 import { getBlogPosts } from "@/lib/db/blog";
+import { FaRegPenToSquare } from "react-icons/fa6";
 import config from '@/config';
 
-import "@/styles/about/latest-posts.css";
-
-const { about } = config;
-const { subHeader, pronouns } = about;
-const { firstName, lastName } = about;
-const { preferredName } = about;
-const { title } = config;
-const { introduction } = config.about;
-const { lifestyles } = about;
-const { programmingLanguage, devOps } = about;
+const { about,
+  title
+} = config;
+const {
+  subHeader, pronouns, firstName,
+  lastName, preferredName, introduction,
+  lifestyles, programmingLanguage, devOps
+} = about;
 
 export const metadata: Metadata = {
   title: title,
@@ -38,11 +33,11 @@ const About = async () => {
   let allBlogs = await getBlogPosts();
 
   let selectedPosts = allBlogs
-    .map(post => ({
+    .map((post) => ({
       ...post,
       metadata: {
         ...post.metadata,
-        category: post.metadata.category || 'Uncategorized',
+        category: post.metadata.category || "Uncategorized",
       },
     }))
     .sort((a, b) => {
@@ -52,55 +47,23 @@ const About = async () => {
       return 1;
     });
 
-  if (selectedPosts.length > 5) {
-    selectedPosts = selectedPosts.slice(0, 5);
-  }
-
   return (
     <article>
       <PageHeader header={header} />
       <AboutHeader text={`${subHeader} (${pronouns})`} />
       <AboutText introduction={introduction} />
       <AboutHeader text="$ ls -al Latest Articles" />
-      <section>
-        <ul className="latest-post-list has-scrollbar">
-          {selectedPosts.map((post, index) => (
-            <li
-              key={index}
-              className="latest-post-item active"
-              data-category={post.metadata.category}
-            >
-              <Link href={`/blog/${post.slug}`} rel="noopener noreferrer">
-                <figure className="latest-post-img">
-                  <div className="latest-post-item-icon-box">
-                    <LuEye />
-                  </div>
-                  <Image
-                    src={post.metadata.banner}
-                    alt={post.metadata.alt || "Blog post image"}
-                    width={1600}
-                    height={900}
-                    priority={true}
-                    placeholder="empty"
-                    loading="eager"
-                  />
-                </figure>
-                <h3 className="latest-post-title"><MarkdownRenderer content={post.metadata.title} /></h3>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <LatestArticles posts={selectedPosts} />
       <SeeMoreButton badge="See All Articles" path="/blog" icon={FaRegPenToSquare} />
       <AboutHeader text="$ ls -al GitHub Stats" />
       <GitHubStats />
       <AboutHeader text="$ ls -al Tech Stack" />
-      <TechStack techStack={programmingLanguage}/>
-      <TechStack techStack={devOps}/>
+      <TechStack techStack={programmingLanguage} />
+      <TechStack techStack={devOps} />
       <AboutHeader text="$ ls -al Life Style" />
       <LifeStyles lifestyles={lifestyles} />
     </article>
   );
-}
+};
 
-export default About
+export default About;
