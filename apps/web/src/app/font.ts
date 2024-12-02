2396d7734537a5203dfeaf48110faf3e461aea83
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2024-12-02 22:22:30 +0800

    refactor(font): add Roboto font and update layout to optimize with `next/font` (#556) (#558)

---
+++ apps/web/src/app/font.ts
@@ -0,0 +1,8 @@
+import { Roboto } from "next/font/google";
+
+export const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
