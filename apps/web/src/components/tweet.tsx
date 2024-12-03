118ede274cbab40c9616b8b81dc658f3a28ab073
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-03 18:24:22 +0800

    feat(tweet): add Tweet component with styling and error handling (#565)
    - closed: #565

---
+++ apps/web/src/components/tweet.tsx
@@ -0,0 +1,47 @@
+import { getTweet } from 'react-tweet/api';
+import { Suspense } from 'react';
+import {
+  TweetSkeleton,
+  EmbeddedTweet,
+  TweetNotFound,
+  type TweetProps,
+} from 'react-tweet';
+import '@/styles/tweet.css';
+
+const TweetContent = async ({ id, components, onError }: TweetProps) => {
+  let error;
+  const tweet = id
+    ? await getTweet(id).catch((err) => {
+      if (onError) {
+        error = onError(err);
+      } else {
+        console.error(err);
+        error = err;
+      }
+    })
+    : undefined;
+
+  if (!tweet) {
+    const NotFound = components?.TweetNotFound || TweetNotFound;
+    return <NotFound error={error} />;
+  }
+
+  return <EmbeddedTweet tweet={tweet} components={components} />;
+};
+
+export const ReactTweet = (props: TweetProps) => <TweetContent {...props} />;
+
+export async function Tweet({ id }: { id: string }) {
+  return (
+    <div className="tweet my-6 text-light-gray">
+      <div className={`flex justify-center`}>
+        <Suspense fallback={<TweetSkeleton />}>
+          <ReactTweet id={id} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+// Usage
+// <Tweet id="1457032789883187201" />
