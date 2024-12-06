d86fcc36905bc19022403029b229b3920e8abfe6
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-06 16:19:22 +0800

    refactor(db): modularize MDX data handling by extracting utility functions (#580)

---
+++ apps/web/src/lib/db/utils/parse-frontmatter.ts
@@ -0,0 +1,27 @@
+type Metadata = {
+  title: string;
+  publishedAt: string;
+  summary: string;
+  category?: string;
+  banner: string;
+  alt?: string;
+  image?: string;
+};
+
+export function parseFrontmatter(fileContent: string) {
+  let frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
+  let match = frontmatterRegex.exec(fileContent);
+  let frontMatterBlock = match![1];
+  let content = fileContent.replace(frontmatterRegex, '').trim();
+  let frontMatterLines = frontMatterBlock.trim().split('\n');
+  let metadata: Partial<Metadata> = {};
+
+  frontMatterLines.forEach((line) => {
+    let [key, ...valueArr] = line.split(': ');
+    let value = valueArr.join(': ').trim();
+    value = value.replace(/^['"](.*)['"]$/, '$1'); // Remove quotes
+    metadata[key.trim() as keyof Metadata] = value;
+  });
+
+  return { metadata: metadata as Metadata, content };
+}
