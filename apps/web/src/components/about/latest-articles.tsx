7f46df719e1cd77fb10a13a9cfb4d5e39543fe7e
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-10 02:54:06 +0800

    style(latest-articles): update styles for light theme

--- apps/web/src/components/about/latest-articles.tsx
+++ apps/web/src/components/about/latest-articles.tsx
@@ -82,11 +82,10 @@ const LatestArticles = ({ posts }: { posts: Post[] }) => {
       <div className="z-10 flex  items-center justify-center">
         <div
           className={cn(
-            "group rounded-full border text-base text-white transition-all ease-in hover:cursor-pointer  border-white/5 bg-neutral-900 hover:bg-neutral-800",
-            // "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
+            "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
           )}
         >
-          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out  hover:duration-300 hover:text-neutral-400">
+          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
             <ProgressBarLink href="/post">
               <span>✨ See More Posts</span>
               <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5 relative top-[-2px]" />
