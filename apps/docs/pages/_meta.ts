00c01b237af32afa87708f845e9cb02514fde281
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-11 02:45:10 +0800

    docs: add new "Stack" page and update meta information

--- apps/docs/pages/_meta.ts
+++ apps/docs/pages/_meta.ts
@@ -1,11 +1,12 @@
 export default {
   "index": "Welcome to 1chooo.com",
-  "contributing": "Contributing",
   "user-guide": "User Guide",
+  "stack": "Stack",
   "code-walkthrough": "Code Walkthrough",
   "wiki": "Wiki",
   "change-log": "Change Log",
   "thanks": "Special Thanks",
+  "contributing": "Contributing",
   "demo": {
     title: 'Demo',
     type: 'page',
