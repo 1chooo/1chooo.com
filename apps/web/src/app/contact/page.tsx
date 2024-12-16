f7e06aa52dc95fb491284889b85c82c775143577
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-16 23:53:07 +0800

    refactor(contact): update MapBox component to accept googleMapUrl as a prop for better flexibility

--- apps/web/src/app/contact/page.tsx
+++ apps/web/src/app/contact/page.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
     <article data-page=''>
       <PageHeader header="Hugo's Contact" />
       <section className="mb-[10px]">
-        <MapBox />
+        <MapBox googleMapUrl='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.9398617144193!2d121.19272387485687!3d24.968160640940518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346823c1ec904dcb%3A0xcdc129d4455ce456!2sNational%20Central%20University!5e0!3m2!1sen!2stw!4v1704610088586!5m2!1sen!2stw'/>
         <h3 className="text-white-2 text-2xl font-bold mb-[20px]">Contact Form</h3>
         <form action="#" className="form" data-form>
           <div className="input-wrapper">
--- apps/web/src/components/contact/map-box.tsx
+++ apps/web/src/components/contact/map-box.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "@/styles/contact/map-box.css";
 
-const MapBox = () => {
+interface MapBoxProps {
+  googleMapUrl: string;
+}
+
+function MapBox({ googleMapUrl }: MapBoxProps) {
   return (
-    <section className="mapbox" data-mapbox="true" data-testid="mapbox-section">
+    <section className="mapbox" data-mapbox="true">
       <figure>
         <iframe
           title="map address"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.9398617144193!2d121.19272387485687!3d24.968160640940518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346823c1ec904dcb%3A0xcdc129d4455ce456!2sNational%20Central%20University!5e0!3m2!1sen!2stw!4v1704610088586!5m2!1sen!2stw"
+          src={googleMapUrl}
           width="400"
           height="300"
           loading="lazy"
