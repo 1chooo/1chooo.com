5600b2a4a8fe5c3b34abe26fdd5a64e2d15075a3
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-19 22:50:53 +0800

    refactor(github-stats): simplify component by removing unused state and effects

--- apps/web/src/components/about/github-stats.tsx
+++ apps/web/src/components/about/github-stats.tsx
@@ -1,15 +1,8 @@
-"use client";
-
-import React, { useEffect, useState } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 import { ThemeInput } from 'react-activity-calendar';
 
 import config from '@/config';
 
-/* 
-* goto https://grubersjoe.github.io/react-activity-calendar/
-* to see more themes
-*/
 const yellowTheme: ThemeInput = {
   light: ['hsl(0, 0%, 92%)', '#FFDA6B'],
   dark: ['hsl(0, 0%, 22%)', '#FFDA6B'],
@@ -18,40 +11,17 @@ const yellowTheme: ThemeInput = {
 const { about } = config;
 const { githubUsername } = about;
 
-const MOBILE_CALENDAR_SIZE = 12;
-const LAPTOP_CALENDAR_SIZE = 12;
-const MOBILE_BREAKPOINT = 768;
-
+/**
+ * @link
+ * https://grubersjoe.github.io/react-activity-calendar/
+ */
 function GitHubStats({ }) {
-  const [username, setUsername] = useState<string>(githubUsername);
-  const [windowWidth, setWindowWidth] = useState<number>(0);
-
-  useEffect(() => {
-    // Handle window width and search params client-side
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const user = urlParams.get('user');
-    if (user) {
-      setUsername(user);
-    }
-
-    window.addEventListener('resize', handleResize);
-    handleResize(); // Set initial window width
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const isMobile = windowWidth <= MOBILE_BREAKPOINT;
 
   return (
     <section className="text-light-gray mt-5">
       <GitHubCalendar
-        username={username}
-        blockSize={isMobile ? MOBILE_CALENDAR_SIZE : LAPTOP_CALENDAR_SIZE}
+        username={githubUsername}
+        blockSize={12}
         blockMargin={4}
         colorScheme="dark"
         blockRadius={2}
