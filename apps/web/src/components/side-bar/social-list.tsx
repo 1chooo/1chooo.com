2974acd28c641f4ea34b456d5a1d7372a31b7c30
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-20 15:30:11 +0800

    style(side-bar): apply hover styles directly to list items for improved UI feedback

--- apps/web/src/components/side-bar/social-list.tsx
+++ apps/web/src/components/side-bar/social-list.tsx
@@ -16,10 +16,9 @@ function SocialList({ socialLinks }: SocialListProps) {
   return (
     <ul className="flex justify-start items-center gap-[15px] pb-1 pl-[7px] xl:justify-center md:justify-center">
       {socialLinks.map(({ url, icon: Icon, name }) => (
-        <li key={name}>
+        <li key={name} className="text-light-gray-70 text-lg hover:scale-110 hover:text-orange-yellow-crayola">
           <Link
             href={url}
-            className="text-light-gray-70 text-lg hover:scale-110 hover:text-orange-yellow-crayola"
             target="_blank"
             rel="noopener noreferrer"
             aria-label={name}
