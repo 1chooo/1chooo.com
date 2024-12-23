8ad47a90b403ca6032a42b96fb27240ab087ed2c
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-23 21:46:55 +0800

    style(side-bar): refactor contact item layout and clean up CSS

--- apps/web/src/components/side-bar/contact-item.tsx
+++ apps/web/src/components/side-bar/contact-item.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import IconBox from "../icon-box";
 import ContactInfo from "./contact-info";
-
 import { IconType } from "react-icons";
 
 interface ContactItemProps {
@@ -14,11 +13,11 @@ function ContactItem({
   icon, title, children
 }: ContactItemProps) {
   return (
-    <li className="contact-item">
+    <li className=" min-w-full flex items-center gap-4">
       <IconBox icon={icon} />
       <ContactInfo title={title}>{children}</ContactInfo>
     </li>
   );
-};
+}
 
 export default ContactItem;
