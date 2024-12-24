a33f6c5af647c3f2d0ef0a6277233164770ac299
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-24 22:46:08 +0800

    feat(loading): add loading component for about page and refactor dynamic imports

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,22 +1,12 @@
 import type { Metadata } from "next";
-import dynamic from 'next/dynamic';
 import PageHeader from '@/components/page-header';
 import AboutHeader from '@/components/about/about-header';
 import MarkdownRenderer from "@/components/markdown/markdown-renderer";
 import { getBlogPosts } from "@/lib/db/v1/post";
 import config from '@/config';
-
-const DynamicLatestArticles = dynamic(() => import('@/components/about/latest-articles'), {
-  loading: () => <p>Loading latest articles...</p>,
-});
-
-const DynamicLifeStyles = dynamic(() => import('@/components/about/life-styles'), {
-  loading: () => <p>Loading life styles...</p>,
-});
-
-const DynamicCodingStats = dynamic(() => import('@/components/about/coding-stats'), {
-  loading: () => <p>Loading coding stats...</p>,
-});
+import LatestArticles from '@/components/about/latest-articles';
+import LifeStyles from '@/components/about/life-styles';
+import CodingStats from '@/components/about/coding-stats';
 
 const { about, title } = config;
 const {
@@ -51,11 +41,11 @@ async function About() {
       <AboutHeader text={`${subHeader} (${pronouns})`} />
       <MarkdownRenderer className="text-light-gray leading-relaxed" content={introduction} />
       <AboutHeader text="$ ls -al Latest Articles" />
-      <DynamicLatestArticles posts={selectedPosts} />
-      <DynamicCodingStats techStacks={techStacks} githubUsername={githubUsername} />
-      <DynamicLifeStyles lifestyles={lifestyles} />
+      <LatestArticles posts={selectedPosts} />
+      <CodingStats techStacks={techStacks} githubUsername={githubUsername} />
+      <LifeStyles lifestyles={lifestyles} />
     </article>
   );
-};
+}
 
 export default About;
