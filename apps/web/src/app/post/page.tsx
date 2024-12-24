05123aa8a74b9f791b436c5ddece6a5bfc4cb6b7
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2024-12-24 23:06:56 +0800

    feat(post): implement loading skeleton for blog posts and refactor post component to use Suspense (#652)
    - closed: #318 #325

--- apps/web/src/app/post/page.tsx
+++ apps/web/src/app/post/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Image from "next/image";
 import Balancer from 'react-wrap-balancer'
 import PageHeader from "@/components/page-header";
@@ -9,6 +10,7 @@ import { POSTS_PER_PAGE } from "@/lib/constants";
 import config from "@/config";
 import { ProgressBarLink } from "@/components/progress-bar";
 import { getBlogPosts } from "@/lib/db/v1/post";
+import Loading from "./loading";
 
 const { title } = config;
 
@@ -19,7 +21,7 @@ export const metadata = {
 
 type tParams = Promise<{ tag?: string; page?: string }>;
 
-export default async function Post({ searchParams }: { searchParams: tParams }) {
+async function BlogPosts({ searchParams }: { searchParams: tParams }) {
   const { tag = "All", page = "1" } = await searchParams;
   let allBlogs = await getBlogPosts();
   const blogTags = [
@@ -47,65 +49,75 @@ export default async function Post({ searchParams }: { searchParams: tParams }) 
   );
 
   return (
+
+    <section className="blog-posts">
+      <FilterList path="post" selectedTag={selectedTag} blogTags={blogTags} />
+      <FilterSelectBox path="post" selectedTag={selectedTag} blogTags={blogTags} />
+      <ul className="blog-posts-list">
+        {paginatedBlogs.map((post, index) => (
+          <li
+            key={index}
+            className="blog-post-item active"
+            data-category={post.metadata.category}
+          >
+            <ProgressBarLink href={`/post/${post.slug}`} rel="noopener noreferrer">
+              <figure className="blog-banner-box">
+                <Image
+                  src={post.metadata.banner}
+                  alt={post.metadata.alt || "Blog post image"}
+                  width={1600}
+                  height={900}
+                  priority={false}
+                  placeholder="blur"
+                  loading="eager"
+                  blurDataURL="https://docs.1chooo.com/images/cover-with-1chooo-com.png"
+                />
+              </figure>
+              <div className="blog-content">
+                <div className="blog-meta">
+                  <p className="blog-category">{post.metadata.category}</p>
+                  <span className="dot"></span>
+                  <time dateTime={post.metadata.publishedAt}>
+                    {new Date(post.metadata.publishedAt).toLocaleDateString(
+                      "en-us",
+                      {
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric",
+                      }
+                    )}
+                  </time>
+                </div>
+                <h3 className="text-2xl text-white-2 font-semibold leading-[1.3] transition-all hover:text-orange-yellow-crayola">
+                  <Balancer><MarkdownRenderer content={post.metadata.title} /></Balancer>
+                </h3>
+                <MarkdownRenderer
+                  className="text-light-gray text-s font-light leading-6 overflow-hidden line-clamp-2"
+                  content={post.metadata.summary}
+                />
+              </div>
+            </ProgressBarLink>
+          </li>
+        ))}
+      </ul>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        selectedTag={selectedTag}
+        basePath="/post"
+      />
+    </section>
+
+  );
+}
+
+export default function Post({ searchParams }: { searchParams: tParams }) {
+  return (
     <article>
       <PageHeader header="Hugo's Blog" />
-      <section className="blog-posts">
-        <FilterList path="post" selectedTag={selectedTag} blogTags={blogTags} />
-        <FilterSelectBox path="post" selectedTag={selectedTag} blogTags={blogTags} />
-        <ul className="blog-posts-list">
-          {paginatedBlogs.map((post, index) => (
-            <li
-              key={index}
-              className="blog-post-item active"
-              data-category={post.metadata.category}
-            >
-              <ProgressBarLink href={`/post/${post.slug}`} rel="noopener noreferrer">
-                <figure className="blog-banner-box">
-                  <Image
-                    src={post.metadata.banner}
-                    alt={post.metadata.alt || "Blog post image"}
-                    width={1600}
-                    height={900}
-                    priority={false}
-                    placeholder="blur"
-                    loading="eager"
-                    blurDataURL="https://docs.1chooo.com/images/cover-with-1chooo-com.png"
-                  />
-                </figure>
-                <div className="blog-content">
-                  <div className="blog-meta">
-                    <p className="blog-category">{post.metadata.category}</p>
-                    <span className="dot"></span>
-                    <time dateTime={post.metadata.publishedAt}>
-                      {new Date(post.metadata.publishedAt).toLocaleDateString(
-                        "en-us",
-                        {
-                          month: "long",
-                          day: "numeric",
-                          year: "numeric",
-                        }
-                      )}
-                    </time>
-                  </div>
-                  <h3 className="text-2xl text-white-2 font-semibold leading-[1.3] transition-all hover:text-orange-yellow-crayola">
-                    <Balancer><MarkdownRenderer content={post.metadata.title} /></Balancer>
-                  </h3>
-                  <MarkdownRenderer
-                    className="text-light-gray text-s font-light leading-6 overflow-hidden line-clamp-2"
-                    content={post.metadata.summary}
-                  />
-                </div>
-              </ProgressBarLink>
-            </li>
-          ))}
-        </ul>
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          selectedTag={selectedTag}
-          basePath="/post"
-        />
-      </section>
+      <Suspense fallback={<Loading />}>
+        <BlogPosts searchParams={searchParams} />
+      </Suspense>
     </article>
   );
 }
