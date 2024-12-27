b73515ec6ea16a17fa0cff023aa583cbf1db1738
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-27 18:15:10 +0800

    feat(resume): update DownloadCV component to enhance accessibility and remove unused CSS styles

--- apps/web/src/components/resume/download-cv.tsx
+++ apps/web/src/components/resume/download-cv.tsx
@@ -1,6 +1,5 @@
 import { IoCloudDownloadOutline } from "react-icons/io5";
 import IconBox from "../icon-box";
-import "@/styles/resume/download-cv.css";
 import Link from "next/link";
 
 function DownloadCV() {
@@ -8,7 +7,11 @@ function DownloadCV() {
     <section className="timeline">
       <div className="title-wrapper">
         <IconBox icon={IoCloudDownloadOutline} />
-        <Link href="./cv.pdf" target="_blank" className="hyperlink-highlight">
+        <Link
+          id="download-hugo-cv"
+          href="./cv.pdf"
+          target="_blank"
+          className="text-orange-yellow-crayola underline font-bold flex items-center gap-4 hover:text-opacity-70">
           Download Hugo&apos;s full Curriculum Vitae/Resume (October 2024)
         </Link>
       </div>
