4ea7651bf93dffefc40e2a910ef107ae7e57486c
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-02 19:48:10 +0800

    refactor(icon-box): enhance icon component to support multiple icon types and simplify CSS styles

--- apps/web/src/components/icon-box.tsx
+++ apps/web/src/components/icon-box.tsx
@@ -1,13 +1,14 @@
-import { IconType } from "react-icons";
+import type { Icon } from '@primer/octicons-react';
+import type { IconType } from 'react-icons';
 import "@/styles/icon-box.css";
 
 interface IconBoxProps {
-  icon: IconType;
+  icon: IconType | Icon;
 }
 
 function IconBox({ icon: Icon }: IconBoxProps) {
   return (
-    <div className="icon-box">
+    <div className="icon-box bg-border-gradient-onyx text-orange-yellow-crayola flex items-center justify-center text-lg shadow-shadow-1 relative rounded-xl w-[40px] h-[40px] md:w-[48px] md:h-[48px] md:text-xl z-1 before:bg-eerie-black-1 before:rounded-xl before:absolute before:content-[''] before:inset-[1px]">
       <Icon />
     </div>
   );
