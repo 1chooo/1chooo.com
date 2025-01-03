7a60eff17bfa597d99e3a7e0dd602a4b3cd47441
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-04 00:10:54 +0800

    refactor(web-vitals): remove unused sendToGTM function and its usage to streamline web vitals reporting

--- apps/web/src/components/web-vitals/index.tsx
+++ apps/web/src/components/web-vitals/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useReportWebVitals } from 'next/web-vitals'
-import { sendToGA4, sendToGTM, logMetric } from './web-vital-reports'
+import { sendToGA4, logMetric } from './web-vital-reports'
 
 /**
  * @see https://github.com/madewithnovel/next.js/blob/80185c2dea73ed8a70fe03b75b43e950af3ef7a6/app/vitals.tsx
@@ -11,7 +11,6 @@ export function WebVitals() {
   
   useReportWebVitals((metric) => {
     sendToGA4(metric, GA_ID);
-    sendToGTM(metric);
     logMetric(metric);
   });
 
--- apps/web/src/components/web-vitals/web-vital-reports.ts
+++ apps/web/src/components/web-vitals/web-vital-reports.ts
@@ -15,22 +15,6 @@ export const sendToGA4 = (metric: NextWebVitalsMetric, GA_ID: string | undefined
   }
 };
 
-export const sendToGTM = (metric: NextWebVitalsMetric) => {
-  let metricValue = Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value);
-
-  if (typeof window !== 'undefined' && window.dataLayer) {
-    window.dataLayer.push({
-      event: 'web-vitals',
-      metric_name: metric.name,
-      metric_value: metricValue,
-      metric_id: metric.id,
-    });
-    console.log(`[GTM] Sent metric: ${metric.name}, value: ${metricValue}, id: ${metric.id}`);
-  } else {
-    console.warn('[GTM] dataLayer is not available');
-  }
-};
-
 export const logMetric = (metric: NextWebVitalsMetric) => {
   switch (metric.name) {
     case 'FCP':
