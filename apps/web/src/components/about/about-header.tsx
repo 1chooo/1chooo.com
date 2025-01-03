fa0e7575d414eb2afaa3a5abe5c25272d9fc2295
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-03 15:49:04 +0800

    refactor(about-header): rename Props interface to AboutHeaderProps for clarity

--- apps/web/src/components/about/about-header.tsx
+++ apps/web/src/components/about/about-header.tsx
@@ -1,11 +1,14 @@
-interface Props {
+interface AboutHeaderProps {
   id: string;
   text: string;
 }
 
-function AboutHeader({ id, text }: Props) {
+function AboutHeader({ id, text }: AboutHeaderProps) {
   return (
-    <h2 id={id} className="text-light-gray text-lg font-bold mt-[30px] mb-[30px]">
+    <h2
+      id={id}
+      className="text-light-gray text-lg font-bold mt-[30px] mb-[30px]"
+    >
       <code>{text}</code>
     </h2>
   );
