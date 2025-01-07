7eda3f3c66aad48c1acec08e22c1220c9f0c1ef3
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-07 18:56:29 +0800

    feat(mdx-test): rename test page to mdx-test and update structure

--- apps/web/src/app/mdx-test/page.tsx
+++ apps/web/src/app/mdx-test/page.tsx
@@ -3,8 +3,8 @@ import Welcome from '@/markdown/welcome.mdx'
 // rest of the code will go here.  This is a placeholder.  The prompt did not provide any existing code.
 export default function MyComponent() {
   return (
-    <div>
+    <article>
       <Welcome />
-    </div>
+    </article>
   );
 }
