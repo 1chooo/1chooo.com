9c3985c51f3b276fd1fd23c0356dd01f625220e0
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-07 18:44:07 +0800

    feat(page): add Welcome component to the main page for mdx test

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,6 +5,7 @@ import AboutHeader from '@/components/about/about-header';
 import MarkdownRenderer from "@/components/markdown/markdown-renderer";
 import { getBlogPosts } from "@/lib/db/v1/post";
 import config from '@/config';
+import Welcome from '@/markdown/welcome.mdx'
 import { Suspense } from "react";
 import Script from "next/script";
 
@@ -141,6 +142,7 @@ async function About() {
       <Suspense fallback={<p>Loading latest articles...</p>}>
         <DynamicLifeStyles lifestyles={lifestyles} />
       </Suspense>
+      <Welcome />
     </article>
   );
 };
