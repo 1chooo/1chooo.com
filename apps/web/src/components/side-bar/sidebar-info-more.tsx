3588af71d2b7fadcb01b85c05c5458cc8a786a51
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-01-08 22:45:44 +0800

    feat(side-bar): refactor contact item structure to support optional links and content (#673) (#692)

--- apps/web/src/components/side-bar/sidebar-info-more.tsx
+++ apps/web/src/components/side-bar/sidebar-info-more.tsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import ContactsList from './contact-list';
+import { LuGithub, LuLinkedin, LuMail, LuMapPin } from "react-icons/lu";
 import SocialList from './social-list';
+import type { IconType as ReactIconType } from "react-icons";
+import type { Icon as OcticonsType } from "@primer/octicons-react";
 import Footer from './footer';
 
 import config from "@/config";
 
 const { socialLinks } = config;
 
+interface Contact {
+  icon: ReactIconType | OcticonsType;
+  title: string;
+  content?: string;
+  link?: string;
+}
+
+const contacts: Contact[] = [
+  {
+    icon: LuMapPin,
+    title: "Location",
+    content: "Taipei, Taiwan 🇹🇼",
+  },
+  {
+    icon: LuMail,
+    title: "Email",
+    link: "mailto:hugo970217@gmail.com",
+    content: "hugo970217@gmail.com",
+  },
+  {
+    icon: LuGithub,
+    title: "GitHub",
+    link: "https://github.com/1chooo",
+    content: "1chooo",
+  },
+  {
+    icon: LuLinkedin,
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/1chooo/",
+    content: "Chun-Ho (Hugo) Lin",
+  },
+];
+
 function SideBarInfoMore() {
   return (
     <div className="sidebar-info-more">
       <div className="separator"></div>
-      <ContactsList />
+      <ContactsList contacts={contacts}/>
       <div className="separator-no-line"></div>
       <SocialList socialLinks={socialLinks} />
       <div className="separator-footer"></div>
