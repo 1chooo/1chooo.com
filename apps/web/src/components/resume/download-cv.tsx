a66e14bb83f6ed79357f1e3f640ea3209e697588
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-09 00:43:15 +0800

    feat(resume): simplify CV download text for clarity

--- apps/web/src/components/resume/download-cv.tsx
+++ apps/web/src/components/resume/download-cv.tsx
@@ -18,7 +18,7 @@ function DownloadCV() {
               `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
             )}
           >
-            Download Hugo's full Curriculum Vitae/Resume
+            Download Hugo's full CV
           </span>
 
           <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5 text-white-1" />
