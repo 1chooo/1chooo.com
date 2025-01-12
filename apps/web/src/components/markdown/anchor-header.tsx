93f942497da3293f067efce73b0c2cb32ba9a7ec
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-01-12 17:07:01 +0800

    refactor(posts): improve header formatting and styling in post components (#699) (#704)

--- apps/web/src/components/markdown/anchor-header.tsx
+++ apps/web/src/components/markdown/anchor-header.tsx
@@ -1,6 +1,5 @@
 import React, { JSX } from 'react';
 import slugify from '@/lib/slugify';
-
 import { IoLink } from 'react-icons/io5';
 
 interface HeaderProps {
@@ -8,24 +7,26 @@ interface HeaderProps {
   children: React.ReactNode;
 }
 
-function AnchorHeader ({ level, children, ...props }: HeaderProps) {
+function AnchorHeader({ level, children, ...props }: HeaderProps) {
   let Tag = `h${level}` as keyof JSX.IntrinsicElements;
   let id = slugify(children?.toString() ?? '', { lower: true });
 
   let getMargins = (level: number) => {
     switch (level) {
       case 1:
-        return 'text-3xl font-bold mt-14 mb-10';
+        return 'text-3xl text-white-2 font-bold mt-14 mb-10';
       case 2:
-        return 'text-2xl font-semibold mt-12 mb-8';
+        return 'text-2xl text-white-2 font-bold mt-12 mb-8';
       case 3:
-        return 'text-xl font-medium mt-10 mb-6';
+        return 'text-xl text-white-2 font-bold mt-10 mb-6';
       case 4:
-        return 'text-lg font-medium mt-8 mb-4';
+        return 'text-lg text-white-2 font-semibold mt-8 mb-4';
       case 5:
-        return 'text-base font-medium mt-6 mb-3';
+        return 'text-base text-white-2 font-semibold mt-6 mb-3';
       case 6:
-        return 'text-sm font-medium mt-4 mb-2';
+        return 'text-sm text-white-2 font-semibold mt-4 mb-2';
+      case 7:
+        return 'text-xs text-white-2 font-semibold mt-4 mb-2';
       default:
         return '';
     }
@@ -43,7 +44,7 @@ function AnchorHeader ({ level, children, ...props }: HeaderProps) {
         href={`#${id}`}
         className="no-underline text-[#d6d6d6] hover:underline flex items-center"
       >
-        <span className="absolute left-[-1.5rem] top-[0.2rem] opacity-0 transition-opacity duration-200 ease-in-out scale-90 group-hover:opacity-100">
+        <span className="absolute left-[-2.0rem] top-[0.2rem] opacity-0 transition-opacity duration-200 ease-in-out scale-90 group-hover:opacity-100">
           <IoLink />
         </span>
       </a>
