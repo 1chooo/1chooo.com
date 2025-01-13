cb64f26171d2facd4b1520a8ca97edd75fe4b90b
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-01-13 17:14:37 +0800

    fix(latest-articles): rearrange components for improved structure and readability (#709) (#714)

--- apps/web/src/components/about/latest-articles.tsx
+++ apps/web/src/components/about/latest-articles.tsx
@@ -103,12 +103,12 @@ function LatestArticles({ posts }: LatestArticlesProps) {
             "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
           )}
         >
-          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
-            <ProgressBarLink href="/post" onClick={handleSeeMorePostsClick}>
+          <ProgressBarLink href="/post" onClick={handleSeeMorePostsClick}>
+            <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
               <span>✨ See More Posts</span>
-              <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5 relative top-[-2px]" />
-            </ProgressBarLink>
-          </AnimatedShinyText>
+              <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+            </AnimatedShinyText>
+          </ProgressBarLink>
         </div>
       </div>
     </section>
