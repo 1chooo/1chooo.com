a733586392e0d2d0fc405aab1bc9927528df8f47
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-18 18:57:41 +0800

    feat(gallery): customize AnimatedGradientText component and update color of icon

--- apps/web/src/app/gallery/page.tsx
+++ apps/web/src/app/gallery/page.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import PageHeader from '@/components/page-header';
 import config from '@/config';
 import { BlurFadeDemo } from "@/components/magicui/blur-fade-demo";
-import { AnimatedGradientTextDemo } from "@/components/magicui/animated-gradient-text-demo";
+import Link from "next/link";
+import AnimatedGradientText from "@/components/magicui/animated-gradient-text";
+import { ChevronRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const { title } = config;
 
@@ -10,12 +13,31 @@ export const metadata: Metadata = {
   title: `Gallery | ${title}`,
 };
 
-async function Gallery() {
+function Gallery() {
   return (
     <article>
       <PageHeader header="Hugo's Gallery" />
       <BlurFadeDemo />
-      {await AnimatedGradientTextDemo()}
+      <div className="z-10 flex items-center justify-center mt-5 mb-5">
+        <Link
+          href="https://magicui.design/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AnimatedGradientText>
+            📑 <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "}
+            <span
+              className={cn(
+                `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+              )}
+            >
+              Thanks for Magic UI!
+            </span>
+
+            <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5 text-white-1" />
+          </AnimatedGradientText>
+        </Link>
+      </div>
     </article>
   );
 };
