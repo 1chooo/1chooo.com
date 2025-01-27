13676d21feef18ddcb54a6923aae9b5b5823fba7
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-01-27 15:17:56 +0800

    feat(side-bar): integrate InfoContent to SideBarInfo (#753) (#794)

--- apps/web/src/components/side-bar/sidebar-info.tsx
+++ apps/web/src/components/side-bar/sidebar-info.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import InfoContent from './info-content';
 import InfoMoreButton from './info-more-button';
 import Image from 'next/image';
 import { useResponsiveImageSize } from '@/hooks/use-responsive-image-size';
@@ -14,6 +13,7 @@ interface SideBarInfoProps {
   lastName: string;
   middleName: string;
   preferredName: string;
+  status: string;
 }
 
 function SideBarInfo({
@@ -23,6 +23,7 @@ function SideBarInfo({
   lastName,
   middleName,
   preferredName,
+  status,
 }: SideBarInfoProps) {
   const imageSize = useResponsiveImageSize(breakpoints);
 
@@ -40,7 +41,16 @@ function SideBarInfo({
           priority
         />
       </figure>
-      <InfoContent />
+      <div className="info-content">
+        <h1
+          className="name"
+          title={`${firstName} (${preferredName}) ${lastName}`}
+        >{firstName} ({preferredName}) {lastName}
+        </h1>
+        <p className="text-white-1 bg-onyx text-xs font-light max-w-max rounded-[8px] custom-lg:m-auto px-[12px] py-[3px] md:px-[18px] md:py-[5px]">
+          <strong>{status}</strong>
+        </p>
+      </div>
       <InfoMoreButton onClick={onToggle} />
     </div>
   );
