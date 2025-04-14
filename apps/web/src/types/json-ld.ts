d78303c3b3b9c27be425286d2a17324b1c4641ba
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-04-14 18:19:45 +0800

    docs(web): add comments in json-ld person

--- apps/web/src/types/json-ld.ts
+++ apps/web/src/types/json-ld.ts
@@ -1,14 +1,17 @@
-/**
- * https://json-ld.org/
- * https://www.npmjs.com/package/jsonld
- */
-
 export interface CollegeOrUniversity {
   "@type": "CollegeOrUniversity"
   name: string
   sameAs: string
 }
 
+/**
+ * Schema.org type for Person
+ * 
+ * This is a simplified version of the schema.org Person type.
+ * It includes only the properties that are relevant for this application.
+ * 
+ * @see https://schema.org/Person
+ */
 export interface Person {
   "@context": "http://schema.org"
   "@type": "Person"
