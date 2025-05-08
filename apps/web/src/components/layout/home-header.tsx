29f40d0da56f8433ab2fe793c4079af95d430fd6
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-08 10:29:50 +0800

    refactor(web): correct import order and adjust header z-index for improved layout

--- apps/web/src/components/layout/home-header.tsx
+++ apps/web/src/components/layout/home-header.tsx
@@ -3,8 +3,8 @@
 import React from "react";
 import { usePathname } from "next/navigation";
 import { ProgressBarLink } from "@/components/progress-bar";
-import type { NavigationLink } from "@/types/nav-bar";
 import { BlurFade } from "@/components/magicui/blur-fade";
+import type { NavigationLink } from "@/types/nav-bar";
 
 interface HomeHeaderProps {
   navigationLinks: NavigationLink[];
@@ -21,7 +21,7 @@ function HomeHeader({ navigationLinks }: HomeHeaderProps) {
   };
 
   return (
-    <header className="fixed bottom-0 left-0 w-full backdrop-blur-[10px] bg-[hsla(240,1%,17%,0.75)] border border-[var(--jet)] shadow-xl z-[5] lg:absolute lg:bottom-auto lg:top-0 lg:left-auto lg:right-0 lg:w-max lg:shadow-none lg:px-5 rounded-t-[12px] sm:rounded-t-[20px] lg:rounded-[0_20px]">
+    <header className="fixed bottom-0 left-0 w-full backdrop-blur-[10px] bg-[hsla(240,1%,17%,0.75)] border border-jet shadow-xl z-10 lg:absolute lg:bottom-auto lg:top-0 lg:left-auto lg:right-0 lg:w-max lg:shadow-none lg:px-5 rounded-t-xl sm:rounded-t-[20px] lg:rounded-[0_20px]">
       <BlurFade direction="up">
         <ul className="flex flex-wrap justify-center items-center px-2.5 sm:gap-5 lg:gap-[30px] lg:px-5">
           {navigationLinks.map((link) => (
