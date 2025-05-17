52af063aeaddc474869f1f316cf060ba3701cba3
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-17 19:44:53 +0800

    revert(https://github.com/1chooo/1chooo.com/commit/6d4aec1970c73a96db9c46ee60dd41b17017a8df): switch to standard CSS for PageHeader (#1037)
    - resolved: #1037

--- apps/web/src/components/page-header.tsx
+++ apps/web/src/components/page-header.tsx
@@ -7,9 +7,9 @@ interface PageHeaderProps {
 function PageHeader({ header }: PageHeaderProps) {
   return (
     <header>
-      <h1 className="relative pb-[7px] mb-[30px] font-semibold text-3xl text-white-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-[30px] after:h-[3px] after:rounded-sm  after:bg-text-gradient-yellow sm:pb-[15px] sm:after:w-[40px] sm:after:h-[5px] md:pb-[20px]">
-        {header}
-      </h1>
+      <h2 className="h2 article-title">
+       {header}
+     </h2>
     </header>
   );
 }
