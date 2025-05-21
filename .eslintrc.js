80972e98c84758cc47a6e0286b0cc6502d749b59
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-21 10:09:19 +0800

    fix(eslint): update extends to use react-internal configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@workspace/eslint-config/library.js"],
+  extends: ["@workspace/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
