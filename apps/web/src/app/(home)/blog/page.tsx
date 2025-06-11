408b91e681d4a57c6c75744e8c0bdcce8fdd1ce8
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-06-11 18:33:34 +0800

    feat(web): enhance blog component with post listing and improved styling

--- apps/web/src/app/(home)/blog/page.tsx
+++ apps/web/src/app/(home)/blog/page.tsx
@@ -1,6 +1,11 @@
-import { getAllPosts } from "@/lib/api";
+import Image from "next/image";
+
 import PageHeader from "@/components/page-header";
-import BlogPosts from "@/components/blog/blog-posts";
+import { ProgressBarLink } from "@/components/progress-bar";
+
+import { getAllPosts } from "@/lib/api";
+
+import { Post } from "@/interfaces/post";
 
 import "@/styles/blog.css";
 
@@ -10,7 +15,45 @@ export default function Blog() {
   return (
     <article>
       <PageHeader header="Hugo's Blog" />
-      <BlogPosts allPosts={allPosts} />
+      <section className="blog-posts">
+        <ul className="blog-posts-list">
+          {allPosts.map((post: Post) => (
+            <li key={post.slug} className="blog-post-item">
+              <ProgressBarLink
+                href={`/blog/${post.slug}`}
+                rel="noopener noreferrer"
+              >
+                <figure className="blog-banner-box">
+                  <Image
+                    src={post.coverImage}
+                    alt={post.title || "Blog post image"}
+                    width={1600}
+                    height={900}
+                    priority={false}
+                    placeholder="blur"
+                    loading="eager"
+                    blurDataURL="https://docs.1chooo.com/images/cover-with-1chooo-com.png"
+                  />
+                </figure>
+                <div className="blog-content">
+                  <div className="blog-meta">
+                    <span className="dot"></span>
+                    <time dateTime={post.date}>
+                      {new Date(post.date).toLocaleDateString("en-us", {
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric",
+                      })}
+                    </time>
+                  </div>
+                  <h3 className="blog-item-title">{post.title}</h3>
+                  <p className="blog-text overflow-hidden line-clamp-2">{post.excerpt}</p>
+                </div>
+              </ProgressBarLink>
+            </li>
+          ))}
+        </ul>
+      </section>
     </article>
   );
 }
